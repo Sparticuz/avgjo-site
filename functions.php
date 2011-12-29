c75b3acee9d468173bf0f0eba138681b1b432ca8
Sparticuz <kyle@kmcnally.net>
2011-12-29 15:18:18 +0000

    Added blank Wordpress framework
    git-svn-id: https://haven.kmcnally.net/repos/apollo-avgjo@611 81754248-ed36-4744-880e-386003e8af05

---
+++ functions.php
@@ -0,0 +1,39 @@
+<?php
+	
+	// Add RSS links to <head> section
+	automatic_feed_links();
+
+	/*Add Menu Management & Thumbnail support */
+	if (function_exists('add_theme_support')){
+		add_theme_support('menus');
+		add_theme_support('post-thumbnails');
+	}
+	
+	// Load jQuery from Google
+	if ( !is_admin() ) {
+	   wp_deregister_script('jquery');
+	   wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"), false);
+	   wp_enqueue_script('jquery');
+	}
+	
+	// Clean up the <head>
+	function removeHeadLinks() {
+    	remove_action('wp_head', 'rsd_link');
+    	remove_action('wp_head', 'wlwmanifest_link');
+    }
+    add_action('init', 'removeHeadLinks');
+    remove_action('wp_head', 'wp_generator');
+    
+    if (function_exists('register_sidebar')) {
+    	register_sidebar(array(
+    		'name' => 'Sidebar Widgets',
+    		'id'   => 'sidebar-widgets',
+    		'description'   => 'These are widgets for the sidebar.',
+    		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+    		'after_widget'  => '</div>',
+    		'before_title'  => '<h3>',
+    		'after_title'   => '</h3>'
+    	));
+    }
+
+?>
