9ab1d51ee8621489773fbb040e30982c4ef7f94e
Sparticuz <kyle@kmcnally.net>
2013-02-11 23:12:47 +0000

    added search
    git-svn-id: https://haven.kmcnally.net/repos/apollo-avgjo@664 81754248-ed36-4744-880e-386003e8af05

--- search.php
+++ search.php
@@ -1,21 +1,38 @@
 <?php get_header(); ?>
-<div id="content">
-	<?php if (have_posts()) : ?>
-		<h2>Search Results</h2>
-		<?php include (TEMPLATEPATH . '/inc/nav.php' ); ?>
-		<?php while (have_posts()) : the_post(); ?>
-			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-				<h2><?php the_title(); ?></h2>
-				<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
-				<div class="entry">
-					<?php the_excerpt(); ?>
-				</div>
-			</div>
-		<?php endwhile; ?>
-		<?php include (TEMPLATEPATH . '/inc/nav.php' ); ?>
-	<?php else : ?>
-		<h2>No posts found.</h2>
-	<?php endif; ?>
+<div id="content" class="row shadow">
+	<div class="twelve columns">
+		<div class="post panel">
+			<?php if (have_posts()) : ?>
+				<h2>Search Results</h2>
+				<?php include (TEMPLATEPATH . '/inc/nav.php' ); ?>
+				<ul>	
+					<?php while (have_posts()) : the_post(); ?>
+						<li <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+							<div class="panel clearfix">
+								<?php //this is where the posts are on the category pages ?>
+								<header>
+									<h2 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+									<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
+								</header>
+								<article class="entry">
+									<div class="image"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a></div>
+									<?php the_content("Continue reading " . the_title('', '', false)); ?>
+								</article>
+								<!--<footer class="postmetadata">
+									<?php the_tags('Tags: ', ', ', '<br />'); ?>
+									Posted in <?php the_category(', ') ?> |
+									<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+								</footer>-->
+							</div>
+						</li>
+					<?php endwhile; ?>
+				</ul>
+				<?php include (TEMPLATEPATH . '/inc/nav.php' ); ?>
+			<?php else : ?>
+				<h2>No posts found.</h2>
+			<?php endif; ?>
+		</div>
+	</div>
 </div>
 <?php //get_sidebar(); ?>
 
