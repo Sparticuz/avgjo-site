fc6254bc455e068f600e227efb953237d4e6bb30
Sparticuz <kyle@kmcnally.net>
2013-04-27 18:20:40 +0000

    Right align search label
    git-svn-id: https://haven.kmcnally.net/repos/apollo-avgjo@672 81754248-ed36-4744-880e-386003e8af05

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get" class="nice">
     <div>
-        <label for="s" class="screen-reader-text">Search for:</label>
+        <label for="s" class="screen-reader-text" style="padding-right: 67px; text-align: right;">Search for</label>
         <input type="text" id="s" name="s" value="" class="input-text" style="width:70%;"/>
         <input type="submit" value="Search" id="searchsubmit" class="nice small radius button" />
     </div>
